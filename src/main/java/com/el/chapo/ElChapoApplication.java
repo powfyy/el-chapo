1f7941a388525389313502c62729d626a81931d2
Dmitry Serebryansky <d.srb@yandex.ru>
2024-07-20 16:05:34 +0300

    init commit

---
+++ src/main/java/com/el/chapo/ElChapoApplication.java
@@ -0,0 +1,13 @@
+package com.el.chapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElChapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElChapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/el/chapo/ElChapoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.el.chapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElChapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
